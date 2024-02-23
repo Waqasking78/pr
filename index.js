81fa8f0650b0b8bdd3bc6be7319b841bba47ddda
waqas <ranaw4376@gmail.com>
2024-02-23 16:21:57 +0500

    ad

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const port = process.env.PORT || 3000
 app.get('/' , (req , res)=>{
    res.send('hello from simple server :)')
 })
+app.get('/about' , (req , res)=>{
+   res.send('my name is wakas')
+})
 app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
